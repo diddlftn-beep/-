6bf0cac245f6d24fce428cb81a56a198c0cf8d4a
diddlftn-beep <diddlftn@gmail.com>
2025-12-17 17:51:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="테스트", layout="wide")
-st.title("✅ 살아났습니다!")
-st.write("이 화면이 보이면 프로그램은 멀쩡한 겁니다.")
-st.write("이제 다시 코드를 채워넣으면 됩니다.")
+st.set_page_config(page_title="생존신고")
+st.title("✅ 프로그램 정상 작동 중!")
+st.write("이 화면이 보인다면 코드 문제는 아닙니다.")
